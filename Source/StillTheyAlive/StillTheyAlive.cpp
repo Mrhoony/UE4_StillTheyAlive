152f9f6bcc09376afb6277988e2ac538bb860725
Mrhoony <52897037+Mrhoony@users.noreply.github.com>
2023-01-28 21:11:16 +0900

    Create Project

---
+++ Source/StillTheyAlive/StillTheyAlive.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "StillTheyAlive.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StillTheyAlive, "StillTheyAlive" );
---
+++ Source/StillTheyAlive/StillTheyAliveGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StillTheyAliveGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STILLTHEYALIVE_API AStillTheyAliveGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
