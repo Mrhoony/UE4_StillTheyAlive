c7b67b5e628ae818f79cfa71cc8445e275805b10
ooweaJ <121000013+ooweaJ@users.noreply.github.com>
2023-01-30 21:10:17 +0900

    Player Default (#14)
    * Create BranchTest.txt (#1)
    
    * Create Player (#13)
    
    ---------
    
    Co-authored-by: Mrhoony <52897037+Mrhoony@users.noreply.github.com>

---
+++ Source/StillTheyAlive/Components/CStatusComponent.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "CStatusComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class STILLTHEYALIVE_API UCStatusComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UCStatusComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	FORCEINLINE float GetSneakSpeed() { return SneakSpeed; }
+	FORCEINLINE float GetWalkSpeed() { return WalkSpeed; }
+	FORCEINLINE float GetRunSpeed() { return RunSpeed; }
+	FORCEINLINE bool IsCanMove() { return bCanMove; }
+
+	void SetMove();
+	void SetStop();
+
+// -------------------------------------------------------
+// Variables
+// -------------------------------------------------------
+private:
+	UPROPERTY(EditAnywhere, Category = "Speed") float SneakSpeed = 200;
+	UPROPERTY(EditAnywhere, Category = "Speed")	float WalkSpeed = 400;
+	UPROPERTY(EditAnywhere, Category = "Speed")	float RunSpeed = 600;
+
+private:
+	bool bCanMove = true;
+};
