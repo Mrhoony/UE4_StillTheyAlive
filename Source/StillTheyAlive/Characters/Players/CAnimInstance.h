888b976451fe4fb63f895717284a2b83973bd6c5
Mrhoony <52897037+Mrhoony@users.noreply.github.com>
2023-01-30 21:09:00 +0900

    Create Player (#13)

---
+++ Source/StillTheyAlive/Characters/Players/CAnimInstance.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+
+#include "Components/CActionComponent.h"
+
+#include "CAnimInstance.generated.h"
+
+UCLASS()
+class STILLTHEYALIVE_API UCAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeBeginPlay() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+private:
+	UFUNCTION()
+		void OnActionTypeChanged(EActionType InPrevType, EActionType InNewType);
+
+// -------------------------------------------------------
+// Variables
+// -------------------------------------------------------
+protected:
+	UPROPERTY(BlueprintReadOnly, EditAnywhere) float Speed;
+	UPROPERTY(BlueprintReadOnly, EditAnywhere) float Direction;
+	UPROPERTY(BlueprintReadOnly, EditAnywhere) EActionType ActionType;
+};
--- Source/StillTheyAlive/StillTheyAliveGameModeBase.cpp
+++ Source/StillTheyAlive/StillTheyAliveGameModeBase.cpp
@@ -1,5 +1,7 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
 #include "StillTheyAliveGameModeBase.h"
+#include "Global.h"
 
+AStillTheyAliveGameModeBase::AStillTheyAliveGameModeBase()
+{
+	CHelpers::GetClass<APawn>(&DefaultPawnClass, "Blueprint'/Game/_Project/Characters/Players/BP_CPlayer.BP_CPlayer_C'");
+}
