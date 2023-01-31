b72fb3b2ec515fa7e65158d522c312e9d4edfa3e
Mrhoony <52897037+Mrhoony@users.noreply.github.com>
2023-01-31 23:30:25 +0900

    Squashed commit of the following:
    commit 7126f2d7c8c39c1fa4e169f9a2ddabdd29a6ea03
    Merge: cb8e7f2 e7e9931
    Author: ooweaJ <121000013+ooweaJ@users.noreply.github.com>
    Date:   Tue Jan 31 23:12:33 2023 +0900
    
        Merge branch 'ujw' into dev
    
    commit e7e993147770691dd023f2396d21ec8870968f5b
    Author: ooweaJ <121000013+ooweaJ@users.noreply.github.com>
    Date:   Tue Jan 31 22:29:36 2023 +0900
    
        Create DeckComponent
    
    commit cb8e7f2a9816cd25f0b9a52482b72e951dedb507
    Author: Mrhoony <52897037+Mrhoony@users.noreply.github.com>
    Date:   Mon Jan 30 23:53:57 2023 +0900
    
        Update Player (#15)
    
    commit c7b67b5e628ae818f79cfa71cc8445e275805b10
    Author: ooweaJ <121000013+ooweaJ@users.noreply.github.com>
    Date:   Mon Jan 30 21:10:17 2023 +0900
    
        Player Default (#14)
    
        * Create BranchTest.txt (#1)
    
        * Create Player (#13)
    
        ---------
    
        Co-authored-by: Mrhoony <52897037+Mrhoony@users.noreply.github.com>
    
    commit 5b89d8acb2caedb4fb206779114a22d40d55c8e8
    Author: ooweaJ <121000013+ooweaJ@users.noreply.github.com>
    Date:   Sat Jan 28 22:39:40 2023 +0900
    
        Create BranchTest.txt (#1) (#4)
    
        Co-authored-by: Mrhoony <52897037+Mrhoony@users.noreply.github.com>

---
+++ Source/StillTheyAlive/Perk/CPerk.cpp
@@ -0,0 +1,61 @@
+#include "CPerk.h"
+
+ACPerk::ACPerk()
+{
+
+}
+
+void ACPerk::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ACPerk::L_Action()
+{
+}
+
+void ACPerk::SelectDeck()
+{
+}
+
+void ACPerk::SetUnarmed()
+{
+	SetMode(EPerkType::Unarmed);
+}
+
+void ACPerk::SetWeapon()
+{
+	SetMode(EPerkType::Weapon);
+}
+
+void ACPerk::SetTrap()
+{
+	SetMode(EPerkType::Trap);
+}
+
+void ACPerk::SetSpawn()
+{
+	SetMode(EPerkType::Spawn);
+}
+
+void ACPerk::SetTrinket()
+{
+	SetMode(EPerkType::Trinket);
+}
+
+void ACPerk::SetMode(EPerkType InType)
+{
+	if (Type == InType)
+	{
+		SetUnarmed();
+		return;
+	}
+	ChangeType(InType);
+}
+
+void ACPerk::ChangeType(EPerkType InType)
+{
+	Type = InType;
+}
+
---
+++ Source/StillTheyAlive/Perk/Weapons/CDT_Weapon.cpp
@@ -0,0 +1,2 @@
+#include "CDT_Weapon.h"
+
