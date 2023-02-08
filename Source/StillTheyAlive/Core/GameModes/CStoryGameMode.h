4361f5e0bbccf7be0eddbdf9a4512b04afe18dcd
Mrhoony <52897037+Mrhoony@users.noreply.github.com>
2023-02-09 06:21:11 +0900

    Create GameModes
    Menu, Story, Endless

---
+++ Source/StillTheyAlive/Core/GameModes/CStoryGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CStoryGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STILLTHEYALIVE_API ACStoryGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
