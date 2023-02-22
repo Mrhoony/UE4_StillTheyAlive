60c65231520a62d8d55848ed7c563a5ed0a989e7
Mrhoony <52897037+Mrhoony@users.noreply.github.com>
2023-02-23 05:01:26 +0900

    Merge 3

--- Source/StillTheyAlive/Behavior/CBTService_Spawn.cpp
+++ Source/StillTheyAlive/Behavior/CBTService_Spawn.cpp
@@ -1,28 +1,25 @@
-#include "CBTService_Enemy.h"
+#include "CBTService_Spawn.h"
 #include "Global.h"
-
-#include "Characters/Players/CPlayer.h"
-#include "Characters/Enemies/CEnemy.h"
-#include "Characters/Enemies/CAIController.h"
+#include "Characters/Players/CSpawnCharacter.h"
+#include "Characters/Players/CAIControllerSpawn.h"
 #include "Components/CBehaviorComponent.h"
 #include "Components/CStateComponent.h"
-
 #include "GameFrameWork/Character.h"
 
-UCBTService_Enemy::UCBTService_Enemy()
+UCBTService_Spawn::UCBTService_Spawn()
 {
-	NodeName = "Enemy";
+	NodeName = "Spawn";
 }
 
-void UCBTService_Enemy::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+void UCBTService_Spawn::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
 	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
 
-	ACAIController* controller = Cast<ACAIController>(OwnerComp.GetOwner());
+	ACAIControllerSpawn* controller = Cast<ACAIControllerSpawn>(OwnerComp.GetOwner());
 	CheckNull(controller);
 	UCBehaviorComponent* behavior = CHelpers::GetComponent<UCBehaviorComponent>(controller);
 	CheckNull(behavior);
-	ACEnemy* aiPawn = Cast<ACEnemy>(controller->GetPawn());
+	ACSpawnCharacter* aiPawn = Cast<ACSpawnCharacter>(controller->GetPawn());
 	UCStateComponent* state = CHelpers::GetComponent<UCStateComponent>(aiPawn);
 
 	/*if (state->IsHit())
@@ -30,34 +27,29 @@ void UCBTService_Enemy::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeM
 		behavior->SetHittedMode();
 		return;
 	}*/
-
+	
 	ACharacter* target = behavior->GetTarget();
 	if (target == nullptr)
 	{
-		controller->K2_ClearFocus();
-		if (!behavior->GetLocation().IsNearlyZero())
-		{
-			behavior->SetPatrolMode();
-			return;
-		}
 		behavior->SetWaitMode();
 		return;
 	}
 	else
 	{
-			UCStateComponent* targetState = CHelpers::GetComponent<UCStateComponent>(target);
-			if (targetState->IsDead())
-			{
-				behavior->SetPatrolMode();
-				return;
-			}
+		UCStateComponent* targetState = CHelpers::GetComponent<UCStateComponent>(target);
+		if (targetState->IsDead())
+		{
+			behavior->SetPatrolMode();
+			return;
+		}
 	}
+
 	float distane = aiPawn->GetDistanceTo(target);
 
-	controller->K2_SetFocus(target);
 	if (distane < controller->GetBehaviorRange())
 	{
 		behavior->SetActionMode();
+		
 		return;
 	}
 	else
